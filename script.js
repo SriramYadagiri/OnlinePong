93af4dbeb3494540eb79aa42758e3454f2094d55
UserName835 <73784283+UserName835@users.noreply.github.com>
2021-08-18 10:46:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -75,6 +75,7 @@ function draw(){
         text(players[1].username + " Has Won!", canvas.width/2, canvas.height/2-200, players[0].color, "Arial", 25,);
         cancelAnimationFrame(loop);
         setTimeout(disconnected, 2000);
+        playerCountRef.set(playerCount-1);
     } else if(ball.x+ball.w>canvas.width) {
         text(players[0].username + " Has Won!", canvas.width/2, canvas.height/2, players[1].color, "Arial", 25,);
         cancelAnimationFrame(loop);
